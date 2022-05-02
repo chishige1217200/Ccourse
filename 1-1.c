417edb405a1a98a2a93f180468287756d0e6a65a
chishige1217200 <daichicchi01@gmail.com>
2022-05-02 13:31:22 +0900

    Add: Template

---
+++ 1-1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float a = 0;        //ここを計算式に置き換え
+    printf("%lf\n", a); // aを画面に表示
+
+    return 0; //慣例
+}
---
+++ 1-2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 0;         //ここを計算式に置き換え
+    printf("%d\n", a); // aを画面に表示
+
+    return 0; //慣例
+}
