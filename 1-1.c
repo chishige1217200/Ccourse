930130c47109a8e247a632d688a266c9bd213db4
chishige1217200 <daichicchi01@gmail.com>
2022-05-02 13:40:58 +0900

    Update: 1-1, 1-2 answer

--- 1-1.c
+++ 1-1.c
@@ -2,8 +2,8 @@
 
 int main(void)
 {
-    float a = 0;        //ここを計算式に置き換え
-    printf("%lf\n", a); // aを画面に表示
+    float a = (120 * 3 + 200 * 2) * (1 + 0.08); //ここを計算式に置き換え
+    printf("%lf\n", a);                         // aを画面に表示
 
     return 0; //慣例
 }
