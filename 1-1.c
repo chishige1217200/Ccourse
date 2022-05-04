c80652ea183ab80365359e353eb179fb5a6297bc
chishige1217200 <daichicchi01@gmail.com>
2022-05-04 14:04:16 +0900

    Update: %lfを%fに統一

--- 1-1.c
+++ 1-1.c
@@ -3,7 +3,7 @@
 int main(void)
 {
     float a = (120 * 3 + 200 * 2) * (1 + 0.08); // ここを計算式に置き換え
-    printf("%lf\n", a);                         // aを画面に表示
+    printf("%f\n", a);                          // aを画面に表示
 
     return 0; // 慣例
 }
