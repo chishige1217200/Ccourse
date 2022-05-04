745c41762510555fcaeac32f0430e7b7096a25b6
chishige1217200 <daichicchi01@gmail.com>
2022-05-04 13:33:30 +0900

    Add: signi.c

---
+++ signi.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 123;
+    float b = 0.4567;
+
+    printf("    a: %d\n", a);
+    printf("    b: %.7lf\n", b);
+    printf("a + b: %.7f\n", a + b);
+
+    return 0;
+}
