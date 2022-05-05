7531da41b096804fe8a531ae8e01006d36c982b5
chishige1217200 <daichicchi01@gmail.com>
2022-05-05 18:19:46 +0900

    Update: int_list.cに削除機能追加

--- int_list.c
+++ int_list.c
@@ -3,6 +3,8 @@
 
 void int_list_print();
 void int_list_append(int a);
+void int_list_del(int a);
+// int int_list_sum(); // リストの要素の合計を返す関数（演習用）
 
 struct int_list
 {
@@ -21,6 +23,11 @@ int main(void)
         int_list_append(i);
         int_list_print();
     }
+
+    int_list_del(3);
+    int_list_print();
+    int_list_del(1);
+    int_list_print();
 
     return 0;
 }
@@ -71,6 +78,54 @@ void int_list_append(int a)
                 ne->next = malloc(sizeof(struct int_list *)); // 動的メモリ確保
                 ne->next->num = a;
                 ne->next->next = NULL;
+                return;
+            }
+        }
+    }
+}
+
+void int_list_del(int a)
+{
+    struct int_list *ne = parent;
+    struct int_list *del = NULL;
+    int counter = 1;
+
+    if (a < 1)
+    {
+        fprintf(stderr, "その操作はサポートされていません．\n");
+        return;
+    }
+
+    if (parent == NULL)
+    {
+        fprintf(stderr, "削除する対象がありません．\n");
+        return;
+    }
+    else
+    {
+        while (1)
+        {
+            if (a == 1)
+            {
+                parent = ne->next;
+                free(ne);
+                break;
+            }
+            if (counter == a - 1)
+            {
+                del = ne->next;
+                ne->next = ne->next->next;
+                free(del);
+                break;
+            }
+            if (ne->next != NULL)
+            {
+                ne = ne->next;
+                counter++;
+            }
+            else
+            {
+                fprintf(stderr, "削除する対象がありません．\n");
                 return;
             }
         }
